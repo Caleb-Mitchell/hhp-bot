4f1ce4cfd93d6db24d2e9d247f00b26eae2ec7fc
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2023-12-01 18:39:46 -0600

    Update message listening

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
@@ -11,21 +11,13 @@ client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-// client.on('message', (message) => {
-//   if (message.channel.name === 'inquiry-emails') {
-//     console.log(`Received message in the inquiry-emails channel: ${message.content}`);
-//     message.channel.send('<@&1180254027364175993>');
-//   }
-// });
 
-client.on('messageCreate', async (message) => {
-  // if (message.channel.name === 'inquiry-emails') {
-  // if (message.channelId === '1180237570261667911') {
+client.once('messageCreate', (message) => {
+  if (message.channel.name === 'inquiry-emails') {
     console.log(`Received message in the inquiry-emails channel: ${message.content}`);
-    message.channel.send('<@&1180254027364175993>');
-  // }
+    message.channel.send('@admin');
+  }
 });
-
 
 // Log in to Discord with your client's token
 client.login(process.env.BOT_TOKEN);
