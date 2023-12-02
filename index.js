7fdbc968badd1e028a7fc311c267a609f91a8346
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2023-12-01 19:07:27 -0600

    Update message to use the role mention syntax

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits } = require("discord.js");
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
 
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
 // It makes some properties non-nullable.
-client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-
-client.on('messageCreate', (message) => {
-  if (message.channel.name === 'inquiry-emails') {
+client.on("messageCreate", (message) => {
+  if (message.channel.name === "inquiry-emails") {
     if (message.author.id !== client.user.id) {
       console.log(`Received message in the inquiry-emails channel`);
-      message.channel.send('@admin');
+      // Mention the 'admin' role in the message
+      const adminRole = message.guild.roles.cache.find(
+        (role) => role.name === "admin"
+      );
+
+      if (adminRole) {
+        message.channel.send(`<@&${adminRole.id}>`);
+      } else {
+        console.error('The "admin" role was not found in the server.');
+      }
     }
   }
 });
